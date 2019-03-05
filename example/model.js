b95a4b6c5dde61743ac4aaf85c7f91d169e3d393
Ramesh <rameshchoudhary_s@edgeverve.com>
2019-03-05 18:32:04 +0530

    added test cases and enables CI/CD

--- example/model.js
+++ example/model.js
@@ -4,10 +4,10 @@
 // License text available at https://opensource.org/licenses/MIT
 
 /**
- * 
+ *
  * ©2016-2017 EdgeVerve Systems Limited (a fully owned Infosys subsidiary),
  * Bangalore, India. All Rights Reserved.
- * 
+ *
  */
 
 'use strict';
@@ -16,29 +16,29 @@ var g = require('strong-globalize')();
 
 var DataSource = require('loopback-datasource-juggler').DataSource;
 
-var config = require('rc')('loopback', { dev: { mongodb: {} } }).dev.mongodb;
+var config = require('rc')('loopback', {dev: {mongodb: {}}}).dev.mongodb;
 
 var ds = new DataSource(require('../'), config);
 
 var Customer = ds.createModel('customer', {
-  seq: { type: Number, id: true },
+  seq: {type: Number, id: true},
   name: String,
-  emails: [{ label: String, email: String }],
+  emails: [{label: String, email: String}],
   age: Number,
 });
 
-Customer.destroyAll(function (err) {
+Customer.destroyAll(function(err) {
   Customer.create(
     {
       seq: 1,
       name: 'John1',
       emails: [
-        { label: 'work', email: 'john@x.com' },
-        { label: 'home', email: 'john@y.com' },
+        {label: 'work', email: 'john@x.com'},
+        {label: 'home', email: 'john@y.com'},
       ],
       age: 30,
     },
-    function (err, customer1) {
+    function(err, customer1) {
       console.log(customer1.toObject());
 
       Customer.create(
@@ -46,20 +46,20 @@ Customer.destroyAll(function (err) {
           seq: 2,
           name: 'John2',
           emails: [
-            { label: 'work', email: 'john2@x.com' },
-            { label: 'home', email: 'john2@y.com' },
+            {label: 'work', email: 'john2@x.com'},
+            {label: 'home', email: 'john2@y.com'},
           ],
           age: 35,
         },
-        function (err, customer2) {
-          Customer.find({ where: { 'emails.email': 'john@x.com' } }, function (
+        function(err, customer2) {
+          Customer.find({where: {'emails.email': 'john@x.com'}}, function(
             err,
             customers
           ) {
             g.log('{{Customers}} matched by {{emails.email}} %s', customers);
           });
 
-          Customer.find({ where: { 'emails.0.label': 'work' } }, function (
+          Customer.find({where: {'emails.0.label': 'work'}}, function(
             err,
             customers
           ) {
@@ -78,11 +78,11 @@ Customer.destroyAll(function (err) {
        });
        */
 
-          Customer.findById(customer1.seq, function (err, customer1) {
+          Customer.findById(customer1.seq, function(err, customer1) {
             console.log(customer1.toObject());
 
             customer1.name = 'John';
-            customer1.save(function (err, customer1) {
+            customer1.save(function(err, customer1) {
               console.log(customer1.toObject());
               ds.disconnect();
             });
